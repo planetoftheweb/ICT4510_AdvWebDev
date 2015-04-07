5b70c36672cf3a5e85cecc21c1ab39e3eb8ad0d2
Ray Villalobos <ray@planetoftheweb.com>
2015-04-07 00:26:07 -0400

    Week 02

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,9 +1,34 @@
+/* 
+	name: Ray Villalobos
+ 	course name: ICT 4510 Advanced Web Design and Site Management
+ 	date: 04/06/15
+*/
+
 $(function() {
 
   "use strict";
+  var firstName, lastName, email, phone;
 
-  document.querySelector('#alert').addEventListener('click', function() {
-    alert('Ray Villalobos');
-    console.log('I love JavaScript');
-  });
+  do {
+    firstName = window.prompt("Please enter your first name");
+  } while (firstName === '' && firstName === null);
+
+  do {
+    lastName = window.prompt("Please enter your last name");
+  } while (lastName === '');
+
+  do {
+    email = window.prompt("please enter your email address");
+  } while (email === '');
+
+  do {
+    phone = window.prompt("please enter your phone number");
+  } while (phone === '');
+
+  console.log(firstName, lastName, email, phone);
+  alert('Thanks for filling out the forms ' + firstName);
+
+  if (phone !== null) {
+    alert('We will contact you soon.');
+  }
 });
