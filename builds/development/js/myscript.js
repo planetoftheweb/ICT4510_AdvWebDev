713cff399d80ab501fbdd5d2d779d0c8e3fdc592
Ray Villalobos <ray@planetoftheweb.com>
2015-04-27 13:49:41 -0400

    week05

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,16 +1,11 @@
 /*
   name: Ray Villalobos
   course name: ICT 4510 Advanced Web Design and Site Management
-  date: 04/20/15
+  date: 04/27/15
 */
 
 $(function() {
   'use strict';
-
-  //Animate the form element
-  $( "#myForm" ).animate({
-    opacity: 1
-    }, 1000);
 
   function getUserInfo() {
     var formInfo = {};
@@ -20,7 +15,6 @@ $(function() {
     formInfo.email = $('#email').val();
     formInfo.phone = $('#phone').val();
 
-    myInfoArray = validate(formInfo);
     if (myInfoArray.length === 0) {
         $('#firstname').val('');
         $('#lastname').val('');
@@ -32,37 +26,20 @@ $(function() {
     }
   }
 
-  function validate(myObject) {
-    var myErrors = [];
-    if (myObject.firstname.length === 0) {
-      $('span#error_firstname').html('Please enter your first name').css('color', 'red');
-      myErrors.push('error');
-    } else {
-      $('span#error_firstname').html('');
-    }
-    if (myObject.lastname.length === 0) {
-        $('span#error_lastname').html('Please enter your last name').css('color', 'red');
-        myErrors.push('error');
-    } else {
-      $('span#error_lastname').html('');
-    }
-    if (myObject.email.length === 0) {
-        $('span#error_email').html('Please enter your email').css('color', 'red');
-        myErrors.push('error');
-    } else {
-      $('span#error_email').html('');
-    }
-    return myErrors;
-  }
-
   function display() {
     var info = getUserInfo();
       $('#message').html('<p>' + info + '</p>');
     return info;
   }
 
-  //events
-  $('#submit').click(function() {
+  // Form Validation
+  $( "#myForm" ).validate({
+    submitHandler: function() {
       display();
+      //$(form).ajaxSubmit();
+    }
   });
+
+  // Tabs
+  $( "#tabs" ).tabs();
 });
